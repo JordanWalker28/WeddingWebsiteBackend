48374710361cf82c520d7519a37aa1868ebb1db5
Jordan Walker <jordanswalker28@gmail.com>
2022-08-05 18:34:47 +0100

    removed validation

--- backend/src/validations/user.js
+++ backend/src/validations/user.js
@@ -1,5 +1,5 @@
 import Joi from 'joi';
-const username = Joi.string().alphanum().min(3).max(30).required();
+const username = Joi.string().min(3).max(30).required();
 const diet = "";
 
 export const signUp = Joi.object().keys({
